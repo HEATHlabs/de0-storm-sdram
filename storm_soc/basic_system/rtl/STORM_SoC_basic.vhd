13ad3c33e681da662540bfaf67f4f7d4943646a9
JH <jeremyheath@hotmail.com>
2016-05-12 21:56:54 -0700

    re-added WB_HALT_I

--- storm_soc/basic_system/rtl/STORM_SoC_basic.vhd
+++ storm_soc/basic_system/rtl/STORM_SoC_basic.vhd
@@ -1019,7 +1019,8 @@ begin
 		SDRAM_DQ_IO <= internal_dqo when (internal_dqoe = '1') else "ZZZZZZZZZZZZZZZZ";
 		internal_dqi <= SDRAM_DQ_IO when (internal_dqoe = '0') else "ZZZZZZZZZZZZZZZZ";
 
-		SDRAM_MEM_HALT_O <= '0';-- nothing can go wrong - never ever!
+		SDRAM_MEM_HALT_O <= SDRAM_MEM_STB_I and (not SDRAM_MEM_ACK_O);
+--		SDRAM_MEM_HALT_O <= '0';-- nothing can go wrong - never ever!
 		SDRAM_MEM_ERR_O <= '0';-- nothing can go wrong - never ever!
 --		SDRAM_MEM_CTI_I <= '0';
 --		XMEM_RST <= MAIN_RST ;
